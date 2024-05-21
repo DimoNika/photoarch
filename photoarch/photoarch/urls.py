24216d259e4094634bfed202dbbc915d4b047bac
dima <1@dima.com>
2024-05-22 01:21:11 +0300

    Zamitka file added, urls adjusted

--- photoarch/photoarch/urls.py
+++ photoarch/photoarch/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from photocore.views import index
+from django.urls import path, include
+from photocore import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index),
+    path("admin/", admin.site.urls),
+    path("", include("photocore.urls"))
 ]
