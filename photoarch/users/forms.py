9f7da13faa44672514ce94f30cefaad880078441
dima <1@dima.com>
2024-05-23 02:57:52 +0300

    Designed page for home page and login form, login works on form

--- photoarch/users/forms.py
+++ photoarch/users/forms.py
@@ -3,6 +3,16 @@ from django import forms
 
 class LoginUserForm(forms.Form):
     username = forms.CharField(label="Username",
-                                widget=forms.widgets.TextInput(attrs={"class": "form-input"}))
+        widget=forms.widgets.TextInput(attrs={"class": "form-control", 
+                                              
+                                              "type":"username", 
+                                              "id":"username", 
+                                              "placeholder":"Username"}))
+    
+
     password = forms.CharField(label="Password",
-                                widget=forms.widgets.PasswordInput(attrs={"class": "form-input"}))
+        widget=forms.widgets.PasswordInput(attrs={"class": "form-control", 
+                                                  
+                                                  "type":"password", 
+                                                  "id":"password", 
+                                                  "placeholder":"Password"}))
