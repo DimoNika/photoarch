8a25a4eee28586ab303c8d8a03e2711062bcd20a
dima <1@dima.com>
2024-05-22 13:56:13 +0300

    First blank page added

--- photoarch/photocore/views.py
+++ photoarch/photocore/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("This is main page")
+    # return HttpResponse("This is main page")
+    return render(request=request, template_name=("photocore/index.html"))
