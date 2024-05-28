33b2e7cbc3eb0d5e88fe85242008e00211f8a66a
dima <1@dima.com>
2024-05-29 01:27:03 +0300

    Multiple UI ajustments

--- photoarch/photocore/forms.py
+++ photoarch/photocore/forms.py
@@ -9,7 +9,7 @@ class UploadFilesForm(forms.ModelForm):
     
     def __init__(self, *args, **kwargs):
         super(UploadFilesForm, self).__init__(*args, **kwargs)
-        self.fields['file'].widget.attrs.update({'accept':'.jpg,.jpeg,.png,.gif',
+        self.fields['file'].widget.attrs.update({'accept':'.3gp,.avi,.mov,.m4v,.m4a,.mp3,.mkv,.ogm,.ogg,.oga,.webm,.wav,.bmp,.gif,.jpg,.jpeg,.png,.webp',
                                                     'multiple': 'multiple',
                                                     'class':'form-control',
                                                     'type':'file',
