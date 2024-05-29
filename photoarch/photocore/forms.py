7fd8a425c890b22988712e88c875965b3581aeda
dima <1@dima.com>
2024-05-29 14:25:02 +0300

    File type checking for uploding files applied, UI improvvments

--- photoarch/photocore/forms.py
+++ photoarch/photocore/forms.py
@@ -6,10 +6,11 @@ class UploadFilesForm(forms.ModelForm):
     class Meta:
         model = UploadFile
         fields = ['file']
-    
+
+    # allowed_extensions = ['.bmp', '.gif', '.jpg', '.jpeg', '.png', '.webp','.mp4', '.webm',  '.ogg']
     def __init__(self, *args, **kwargs):
         super(UploadFilesForm, self).__init__(*args, **kwargs)
-        self.fields['file'].widget.attrs.update({'accept':'.3gp,.avi,.mov,.m4v,.m4a,.mp3,.mkv,.ogm,.ogg,.oga,.webm,.wav,.bmp,.gif,.jpg,.jpeg,.png,.webp',
+        self.fields['file'].widget.attrs.update({'accept':'.bmp, .gif, .jpg, .jpeg, .png, .webp,.mp4, .webm,  .ogg',
                                                     'multiple': 'multiple',
                                                     'class':'form-control',
                                                     'type':'file',
--- photoarch/photocore/views.py
+++ photoarch/photocore/views.py
@@ -3,8 +3,13 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .forms import UploadFilesForm
 from .models import UploadFile
 from django.urls import reverse
+import os
 # Create your views here.
 
+# Custom func for extention of files
+def get_file_extension(file_path):
+    _, file_extension = os.path.splitext(file_path)
+    return file_extension
 
 def index(request):
     # return HttpResponse("This is main page")
@@ -22,17 +27,42 @@ def index(request):
 def storage_view(request):
     
     if request.method == "POST":
-        
+
+        allowed_extensions = ['.bmp', '.gif', '.jpg', '.jpeg', '.png', '.webp','.mp4', '.webm',  '.ogg']
+
         form = UploadFilesForm(request.POST, request.FILES)
         if form.is_valid():            
             # Iterate over the files in the request
             for f in request.FILES.getlist('file'):
-                # Save each file individually
-                fp = UploadFile(file=f, owner=request.user)
-                fp.save()
+            
+                # Check if file is allowed
+                if any(f.name.endswith(ext) for ext in allowed_extensions):
+                    # Save each file individually
+                    fp = UploadFile(file=f, owner=request.user)
+                    fp.save()
         return HttpResponseRedirect(reverse("storage"))
 
+    files =  UploadFile.objects.filter(owner=request.user, is_deleted=False),
+    files_with_types = []
+    
+
+    for i in files:
+    
+        for j in i:
+
+            if get_file_extension(str(j.file)) in ['.bmp', '.gif', '.jpg', '.jpeg', '.png', '.webp']:
+                files_with_types.append((j, "picture"))
+            elif get_file_extension(str(j.file)) in [".mp4", ".webm",  ".ogg"]:
+                files_with_types.append((j, "video"))
+            else:
+                files_with_types.append((j, "unknown"))
+    
+                
+                # ['.bmp', '.gif', '.jpg', '.jpeg', '.png', '.webp'];
+                # [".mp4", ".webm",  ".ogg"]
+            
     data = {
+        "files_with_types": files_with_types,
         "files" : UploadFile.objects.filter(owner=request.user, is_deleted=False),
         "is_logged": request.user.is_authenticated,
         "request": request,
